9882d93538d9356c8ab9ee4d903941adfaf8a7c6
Matthew Bridges <mattbridgesbusiness@gmail.com>
2024-07-18 18:06:53 -0700

    feat: fetch some rudimentary data in loader

---
+++ packages/client/src/pages/userSettings/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route } from "../../routes/user.settings";
+
+export const UserSettingsPage: React.FC = () => {
+  const {
+    me: {
+      user: { email, id, username },
+    },
+  } = Route.useLoaderData({
+    select: (data) => {
+      return data;
+    },
+  });
+
+  return (
+    <ul>
+      <li>{email}</li>
+      <li>{id}</li>
+      <li>{username}</li>
+    </ul>
+  );
+};
--- packages/client/src/routes/user.settings.tsx
+++ packages/client/src/routes/user.settings.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import { createTRPCQueryUtils } from "@trpc/react-query";
+import { queryClient, trpcClient } from "../trpc";
+import { UserSettingsPage } from "../pages/userSettings";
 
 export const Route = createFileRoute("/user/settings")({
-  component: () => <div>Hello /user/settings!</div>,
-  loader: () => {},
+  component: () => <UserSettingsPage />,
+  loader: async (ctx) => {
+    const clientUtils = createTRPCQueryUtils({
+      queryClient,
+      client: trpcClient,
+    });
+
+    const me = await clientUtils.user.getMe.ensureData();
+
+    return {
+      me,
+    };
+  },
 });
